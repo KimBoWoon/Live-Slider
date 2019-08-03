4ad63eb10e594e485271127ef560d976acfcfb4a
KimBoWoon <haioer@naver.com>
2019-08-03 16:53:18 +0900

    made viewer about major news

--- app/src/main/java/com/bowoon/android/live_slider/fragment/MajorNewsFragment.kt
+++ app/src/main/java/com/bowoon/android/live_slider/fragment/MajorNewsFragment.kt
@@ -1,5 +1,6 @@
 package com.bowoon.android.live_slider.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +8,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
 import com.bowoon.android.live_slider.R
+import com.bowoon.android.live_slider.activity.WebViewActivity
 import com.bowoon.android.live_slider.databinding.MajorNewsItemBinding
+import com.bowoon.android.live_slider.listener.ItemClickListener
 import com.bowoon.android.live_slider.model.Item
 import com.bowoon.android.live_slider.module.GlideApp
 import com.bumptech.glide.Glide
@@ -41,6 +44,17 @@ class MajorNewsFragment : Fragment() {
                 .centerCrop()
                 .placeholder(R.mipmap.image_not_found)
                 .into(binding.majorNewImage)
+
+            binding.callback = itemClicked
+            binding.item = news
+        }
+    }
+
+    private val itemClicked = object : ItemClickListener {
+        override fun onClick(item: Item) {
+            val intent = Intent(context, WebViewActivity::class.java)
+            intent.putExtra("item", item)
+            startActivity(intent)
         }
     }
 }
